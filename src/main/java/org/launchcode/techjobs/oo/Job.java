60ed0fd66908537c84315f6f064e133a229c4b37
Hunter <hacain98@gmail.com>
2023-05-08 17:23:29 -0500

    part 3 commit

--- src/main/java/org/launchcode/techjobs/oo/Job.java
+++ src/main/java/org/launchcode/techjobs/oo/Job.java
@@ -6,6 +6,7 @@ public class Job {
 
     private int id;
     private static int nextId = 1;
+    private String value;
 
     private String name;
     private Employer employer;
@@ -17,9 +18,72 @@ public class Job {
     //  other five fields. The second constructor should also call the first in order to initialize
     //  the 'id' field.
 
+    public Job() {
+        id = nextId;
+        nextId++;
+    }
+
+    public Job(String name, String employer, String location, String positionType, String coreCompetency) {
+        // help
+    }
+
     // TODO: Add custom equals and hashCode methods. Consider two Job objects "equal" when their id fields
     //  match.
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Job)) return false;
+        Job job = (Job) o;
+        return getId() == job.getId();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+
     // TODO: Add getters for each field EXCEPT nextId. Add setters for each field EXCEPT nextID
     //  and id.
+
+    public int getId() {
+        return id;
+    }
+    //
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    //
+    public String getEmployer() {
+        return employer;
+    }
+    public void setEmployer(String employer) {
+        this.employer = employer;
+    }
+    //
+    public String getLocation() {
+        return location;
+    }
+    public void setLocation(String location) {
+        this.location = location;
+    }
+    //
+    public String positionType() {
+        return positionType;
+    }
+    public void setPositionType(String positionType) {
+        this.positionType = positionType;
+    }
+    //
+    public String coreCompetency() {
+        return coreCompetency;
+    }
+    public void setCoreCompetency(String coreCompetency) {
+        this.coreCompetency = coreCompetency;
+    }
 }
+
+// got to part 4) using the unit test
